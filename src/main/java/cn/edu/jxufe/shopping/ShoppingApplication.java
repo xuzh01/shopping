739dd73ef8efe152e48203b69cc610a40b1b526c
xuzh01 <1047615304@qq.com>
2018-08-06 19:42:26 +0800

    修改版

--- src/main/java/cn/edu/jxufe/shopping/ShoppingApplication.java
+++ src/main/java/cn/edu/jxufe/shopping/ShoppingApplication.java
@@ -1,9 +1,11 @@
 package cn.edu.jxufe.shopping;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.edu.jxufe.shopping.mapper")
 public class ShoppingApplication {
 
     public static void main(String[] args) {
