98d23409e3107cfa147a90d2ca98733bf25419a9
cgg <891842749@qq.com>
2018-08-06 19:59:43 +0800

    基本项目结构。。。

--- src/main/java/cn/edu/jxufe/shopping/service/dsa.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.jxufe.shopping.service;
-
-/**
- * @Author cgg 891842749@qq.com
- * @Date 2018-08-06 19:58:48
- * @Description gitee:www.gitee.com/cgggitee/
- */
-public class dsa {
-}
